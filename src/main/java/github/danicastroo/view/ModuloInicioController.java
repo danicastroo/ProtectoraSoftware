aa2879c7aa83138fa136ed73f27c577420b776d4
danicastro <danicvillalba@gmail.com>
2025-05-12 14:18:15 +0200

    Implementación de la pantalla inicio

--- src/main/java/github/danicastroo/view/ModuloInicioController.java
+++ src/main/java/github/danicastroo/view/ModuloInicioController.java
@@ -1,12 +1,17 @@
 package github.danicastroo.view;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.web.WebView;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ModuloInicioController extends Controller implements Initializable {
+    @FXML
+    private WebView redes;
+
     @Override
     public void onOpen(Object input) throws IOException {
 
@@ -18,7 +23,17 @@ public class ModuloInicioController extends Controller implements Initializable 
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
+    public void initialize(URL location, ResourceBundle resources) {
+        // Cargar el archivo redes.html en el WebView
+        try {
+            URL url = getClass().getResource("/github/danicastroo/images/redes.html");
+            if (url != null) {
+                redes.getEngine().load(url.toExternalForm());
+            } else {
+                System.err.println("No se pudo encontrar el archivo redes.html");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
